b3e9cacf8936525674515678bf73a9ffb1c15ed5
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-03 13:54:59 -0500

    Loop for PostIndex component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function Header() {
@@ -34,30 +31,20 @@ function PostsNew() {
 }
 
 // index moved into its own function
-function PostsIndex() {
+function PostsIndex(props) {
+  console.log(props);
   return (
-    /* index of blog posts */
     <div id="posts-index">
-      <h1>All posts</h1>
-      {/* first post */}
-      <div className="posts">
-        <h2>I Quit My Job</h2>
-        <img
-          src="https://plus.unsplash.com/premium_photo-1668007659236-30c7362ee7bd?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2Fsa2luZyUyMHRyYWlsfGVufDB8fDB8fHww"
-          alt="image of walking path"
-        />
-        <p>The decision didn't feel like a choice between...</p>
-        <button>read more</button>
-      </div>
-      <div className="posts">
-        <h2>So What Is Next?</h2>
-        <img
-          src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHMlMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D"
-          alt="hands at a laptop keyboard"
-        />
-        <p>My journey has taken an unexpected turn...</p>
-        <button>read more</button>
-      </div>
+      <h2>All My Posts!</h2>
+      {/* loop of defined post data props from the parent component */}
+      {props.posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <img src={post.image} alt="blog post image" />
+          <p>{post.body}</p>
+          <button>Read more!</button>
+        </div>
+      ))}
     </div>
   );
 }
@@ -75,10 +62,34 @@ function Footer() {
 
 // creates a content function to import recipe components
 function Content() {
+  let posts = [
+    {
+      id: 1,
+      title: "I Quit My Job",
+      body: "The decision didn't feel like a choice between...",
+      image:
+        "https://plus.unsplash.com/premium_photo-1668007659236-30c7362ee7bd?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2Fsa2luZyUyMHRyYWlsfGVufDB8fDB8fHww",
+    },
+    {
+      id: 2,
+      title: "So What Is Next?",
+      body: "My journey has taken an unexpected turn...",
+      image:
+        "https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHMlMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+      id: 3,
+      title: "What I'm Most Excited About",
+      body: "Most people look a bit shocked...",
+      image:
+        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D",
+    },
+  ];
+
   return (
     <div>
       <PostsNew />
-      <PostsIndex />
+      <PostsIndex posts={posts} />
     </div>
   );
 }
