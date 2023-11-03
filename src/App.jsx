8029a5a62a5d74417fdd53b96c53b1523ca58899
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-03 12:47:15 -0500

    structure of one page app

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,57 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+      </header>
+
+      {/* new blog post form */}
+      <div id="posts-new">
+        <h1>New post</h1>
+        <form>
+          <div>
+            Title: <input type="text" />
+          </div>
+          <div>
+            Body: <input type="text" />
+          </div>
+          <div>
+            Image: <input type="text" />
+          </div>
+          <button type="submit">Create the post!</button>
+        </form>
       </div>
-      <h1>Emily is Awesome</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* index of blog posts */}
+      <div id="posts-index">
+        <h1>All posts</h1>
+        {/* first post */}
+        <div className="posts">
+          <h2>I Quit My Job</h2>
+          <img
+            src="https://plus.unsplash.com/premium_photo-1668007659236-30c7362ee7bd?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2Fsa2luZyUyMHRyYWlsfGVufDB8fDB8fHww"
+            alt="image of walking path"
+          />
+          <p>The decision didn't feel like a choice between...</p>
+          <button>read more</button>
+        </div>
+        <div className="posts">
+          <h2>So What Is Next?</h2>
+          <img
+            src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHMlMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D"
+            alt="hands at a laptop keyboard"
+          />
+          <p>My journey has taken an unexpected turn...</p>
+          <button>read more</button>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+
+      <footer>
+        <p>Copyright 20XX</p>
+      </footer>
+    </div>
   );
 }
 
