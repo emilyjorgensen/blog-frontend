fc5b7ad141c97d05a96fa5b54148d1b36b23bccb
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-07 13:52:49 -0600

    Index Posts tested with button

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,9 +2,18 @@ import { useState } from "react";
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 import { Modal } from "./Modal";
+import axios from "axios";
 
 // creates a content function to import recipe components
 export function Content() {
+  let posts = [];
+
+  const handleIndexPosts = () => {
+    axios.get("http://localhost:3000/posts.json").then((response) => {
+      console.log(response.data);
+    });
+  };
+
   // giving react the variable and the ability to set that variable
   const [isPostsShowVisible, setIsPostsShowVisible] = useState(false);
 
@@ -18,33 +27,10 @@ export function Content() {
     setIsPostsShowVisible(false);
   };
 
-  let posts = [
-    {
-      id: 1,
-      title: "I Quit My Job",
-      body: "The decision didn't feel like a choice between...",
-      image:
-        "https://plus.unsplash.com/premium_photo-1668007659236-30c7362ee7bd?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2Fsa2luZyUyMHRyYWlsfGVufDB8fDB8fHww",
-    },
-    {
-      id: 2,
-      title: "So What Is Next?",
-      body: "My journey has taken an unexpected turn...",
-      image:
-        "https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFuZHMlMjBjb21wdXRlcnxlbnwwfHwwfHx8MA%3D%3D",
-    },
-    {
-      id: 3,
-      title: "What I'm Most Excited About",
-      body: "Most people look a bit shocked...",
-      image:
-        "https://images.unsplash.com/photo-1531297484001-80022131f5a1?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGVjaHxlbnwwfHwwfHx8MA%3D%3D",
-    },
-  ];
-
   return (
     <div>
       <PostsNew />
+      <button onClick={handleIndexPosts}>LOAD POSTS</button>
       <PostsIndex posts={posts} onShowPost={handleShowPost} />
       <Modal show={isPostsShowVisible} onClose={handleClose}>
         <p>TEST</p>
