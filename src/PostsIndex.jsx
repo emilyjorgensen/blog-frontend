fb1979b2c56f3f9632966fe1acfee0fa9985bc39
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-11-09 12:41:39 -0600

    index loop moved into bootstrap cards

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -4,8 +4,24 @@ export function PostsIndex(props) {
   return (
     <div id="posts-index">
       <h2>All My Posts!</h2>
-      {/* loop of defined post data props from the parent component */}
       <div className="posts">
+        {props.posts.map((post) => (
+          <div key={post.id} className="post">
+            <div className="card mb-3">
+              <img src={post.image} className="card-img-top" alt="blog post image" />
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <button onClick={() => props.onShowPost(post)}>Read more!</button>
+                <p className="card-text">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* OLD loop of defined post data props from the parent component */}
+      {/* <div className="posts">
         {props.posts.map((post) => (
           <div key={post.id} className="post">
             <h3>{post.title}</h3>
@@ -13,7 +29,7 @@ export function PostsIndex(props) {
             <button onClick={() => props.onShowPost(post)}>Read more!</button>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
